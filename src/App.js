134bc9d96ec09ebe5ae0c335792cbc65b5fac184
Si Lam <silam@hotmail.com>
2023-10-14 13:16:20 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square({value})
+{
+  return <button className="square">{value}</button>;
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+    <div className="board-row">
+        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
+    </div>
+    <div className="board-row">
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
+    </div>
+    <div className="board-row">
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
+    </div>
+
+    </>
+  );
+ 
+   
+}
