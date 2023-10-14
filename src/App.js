cd675786876a7e3499085c687faa4e65f92e7f59
Si Lam <silam@hotmail.com>
2023-10-14 15:07:43 -0500

    Create an array of 9 elements with null value first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-function Square()
+function Square({value})
 {
-  const [value, setValue] = useState(null);
+  //const [value, setValue] = useState(null);
 
   function handleClick()
   {
-    setValue("X");
-    
+    //setValue("X");
+
     console.log("Clicked " + value);
   }
   return (<button className="square" onClick={handleClick}>
@@ -16,22 +16,25 @@ function Square()
 }
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
     <div className="board-row">
-        <Square></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
     </div>
     <div className="board-row">
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
     </div>
     <div className="board-row">
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
     </div>
 
     </>
