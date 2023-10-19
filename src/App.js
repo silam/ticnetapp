333f587b0f54274f2aa796ac573d71bc1f143689
Si Lam <silam@hotmail.com>
2023-10-19 09:13:29 -0500

    Declare a winner

--- src/App.js
+++ src/App.js
@@ -17,16 +17,42 @@ function Square({value, onSquareClick})
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+ 
+
+  /////////////////////////////////////////////
   function handleClick(i)  {
+    
+    if ( squares[i] || calculateWinner(squares))
+      return;
+    
     const nextSquare = squares.slice();
-    nextSquare[i] = "X";
+
+    if ( xIsNext)
+      nextSquare[i] = "X";
+    else
+      nextSquare[i] = "O";
+
+    setXIsNext(!xIsNext);
     setSquares(nextSquare);
   }
 
+   // calculate winner
+
+   const winner = calculateWinner(squares );
+   let status;
+ 
+   if (winner)
+     status = "Winner: " + winner;
+   else
+     status = "Next player: " + (xIsNext?'X':'O');
+
+     
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
@@ -47,4 +73,28 @@ export default function Board() {
   );
  
    
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+    
+  for ( let i = 0; i< lines.length; i++)
+  {
+    const [a,b,c] = lines[i];
+    if ( squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    {
+        return squares[a];
+    }
+  }
+
+  return null;
 }
