899a0af7817fec71fcde1060f5072e88338348fe
Si Lam <silam@hotmail.com>
2023-10-28 20:53:20 -0500

    Adding AWS authentication into the APP

--- src/App.js
+++ src/App.js
@@ -9,27 +9,62 @@ import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
-export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
-  
+function Game({signOut, user}) {
+
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
     function handlePlay(nextSquares) {
-      setHistory([...history, nextSquares]);
-      setXIsNext(!xIsNext);
+      const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+      setHistory(nextHistory);
+      setCurrentMove(nextHistory.length - 1);
     }
   
+    function jumpTo(nextMove){
+      // TODO
+      setCurrentMove(nextMove);
+
+    }
+
+    const moves = history.map((squares, move)=>
+    {
+      let description;
+      if ( move > 0) {
+        description = 'Go to move #' + move;
+      }
+      else{
+        description = 'Go to game start'
+      }
+
+      return (
+        <li key={move}>
+          <button onClick={()=>jumpTo(move)}>{description}</button>
+        </li>
+      )
+    })
     return (
-      <div className="game">
+      <>
+        <Heading level={1}>Hello {user.username}! Welcome to TicTacToe Game Online</Heading>
+
+        <Button className='button' onClick={signOut}>Sign out</Button>
+        <br></br><br></br><br></br>        <br></br><br></br><br></br>
+
+        <div className="game">
+     
         <div className="game-board">
           <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
         </div>
         <div className="game-info">
-          <ol>{/*TODO*/}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
+      </>
+      
     );
   }
 
+  export default withAuthenticator(Game);
 
 
